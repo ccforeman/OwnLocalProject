d59fda2af0576c52acd1980a3483a36762611020
ccforeman <cameronforeman@live.com>
2016-04-16 01:16:14 -0500

    BusinessGateway uses sqlite3 syntax, takes min value from max and amount

--- WebContent/BusinessGateway.class.php
+++ WebContent/BusinessGateway.class.php
@@ -3,46 +3,41 @@ class BusinessGateway {
 	private $db;
 	
 	public function __construct() {
-		$this->db = Database::getDB();
+		$this->db = new Database();
 	}
 	
-	public function fetchAllBusinesses($amount, $start) {
+	/* Get the records specified by pagification data and make sure not to exceed maximum allowed records */
+	public function fetchBusinessesList($pagification) {
 		$business_array = array();
-		$amount = (empty($amount)) ? 50 : $amount;
-		$start = (empty($start)) ? 0 : $start;
 		
-		try {
-			$this->db = Database::getDB();
-			$query = "SELECT * FROM business LIMIT :amount";
-			$statement = $this->db->prepare($query);
-			$statement->bindValue(":amount", $amount);
-			$statement->execute();
-			while($business = $statement->fetch(PDO::FETCH_ASSOC)) {
-				array_push($business_array, $business);
-			}
-			
-			$statement->closeCursor();
-		} catch (PDOException $e) {
-			die($e->getMessage() . "\n");
+		$query = "SELECT * FROM business LIMIT :start, :amount";
+		$statement = $this->db->prepare($query);
+		$statement->bindValue(":start", $pagification['page_start']);
+		$statement->bindValue(":amount", min($pagification['amount'], $pagification['max']));
+		$rs = $statement->execute();
+		
+		while($business = $rs->fetchArray(SQLITE3_ASSOC)) {
+			array_push($business_array, $business);
 		}
 		
-		return array("business" => $business_array);
+		$statement->close();
+		
+		return $business_array;
 	}
 	
 	public function fetchBusiness($id) {
-		try {
-			$this->db = Database::getDB();
-			$query = "SELECT * FROM business WHERE id = :id";
-			$statement = $this->db->prepare($query);
-			$statement->bindValue(":id", $id);
-			$statement->execute();
-			$business = $statement->fetchAll(PDO::FETCH_ASSOC);
-			$statement->closeCursor();
-		} catch (PDOException $e) {
-			die("fetchBusiness failed\n");
-		}
+		$query = "SELECT * FROM business WHERE id = :id";
+		$statement = $this->db->prepare($query);
+		$statement->bindParam(":id", $id);
+		$rs = $statement->execute();
+		$business = $rs->fetchArray(SQLITE3_ASSOC);
+		$statement->close();
 		
-		return $business[0];
+		if($business == FALSE) {					/* A business that returns false means it wasn't in the database */
+			$errors = new Error(404, "The business was not found");
+			return $errors->getError();
+		}
+		return $business;
 	}
 	
 }
