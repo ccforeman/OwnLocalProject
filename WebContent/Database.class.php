6ecf1386b35caee3c2bdc2950c4897b962758a6b
ccforeman <cameronforeman@live.com>
2016-04-16 01:21:10 -0500

    Database class now only opens and returns database in constructor

--- WebContent/Database.class.php
+++ WebContent/Database.class.php
@@ -1,12 +1,8 @@
 <?php 
 class Database extends SQLite3 {
-	private $db;
 	
 	public function __construct() {
-		
-		if(!isset($this->$db) || $this->$db == null) {		/* Will only open database connection if not already open */
-			$this->open('../ownlocal');
-		}
+		$db = $this->open('../ownlocal');
 	}
 }
 ?>
