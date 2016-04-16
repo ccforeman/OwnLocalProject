1466d9cdd71efb0072e39129899eb35421484f1c
ccforeman <cameronforeman@live.com>
2016-04-16 01:41:45 -0500

    index now handles all json encoding and passes data to GetController

--- WebContent/index.php
+++ WebContent/index.php
@@ -1,19 +1,23 @@
 <?php
 include('ClassLoader.php');
 	header('Content-type: application/json');
+	header('Allow: GET');
+	
 	$req_method = $_SERVER['REQUEST_METHOD'];
 	$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 	list($blank, $uri_object, $arg, $extraneous) = explode("/", $uri);
-	$page_amount = (array_key_exists('amount', $_GET)) ? $_GET['amount'] : null;
-	$page_number = (array_key_exists('page', $_GET)) ? $_GET['page'] : null;
-	
-	if(!empty($extraneous)) {
-		http_response_code(404);
-		die();										/* Messages included with all dies for debugging */
+	$page_start = (array_key_exists('page_start', $_GET)) ? $_GET['page_start'] : null;
+	$amount = (array_key_exists('amount', $_GET)) ? $_GET['amount'] : null;
+
+	switch($req_method) {
+		case 'GET' :
+			$api = new GetController($uri_object, $arg, $extraneous, $page_start, $amount);
+			echo json_encode($api->run($uri_object, $arg), JSON_PRETTY_PRINT ). "\n";
+			break;
+		default :
+			$error = new Error(405, "Only accepting GET requests");
+			echo json_encode( $error->getError(), JSON_PRETTY_PRINT ) . "\n";
+			break;
 	}
-	
-	$api = new MainController($req_method, $uri, $uri_object, $arg, $page_amount, $page_number);
-	$response = $api->run($uri_object, $arg);
-	echo $response . "\n";
 	
 ?>
